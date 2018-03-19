df267481b438864c9f9811048c676022dd919734
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-03-19 03:58:38 -0300

    Update environments

--- src/environments/environment.sample.ts
+++ src/environments/environment.sample.ts
@@ -1,3 +1,7 @@
+// production: environment.prod.ts
+// development: environment.dev.ts
+// default (no env params): environment.ts
+
 export const environment = {
   production: true,
   googleApiKey: ''
